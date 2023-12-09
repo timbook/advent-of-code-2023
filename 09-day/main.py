1ae4fc56b9084ffa1d0ab8e09ae9b9974796245c
Tim Book <timothykbook@gmail.com>
2023-12-09 00:42:11 -0500

    day 9 i am a genius

---
+++ 09-day/main.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.special import comb
+import pdb
+
+#  x = np.loadtxt('data/sample.txt', dtype=int, delimiter=' ')
+x = np.loadtxt('data/input.txt', dtype=int, delimiter=' ')
+R, C = x.shape
+
+def get_d(row):
+    for d in range(1, C):
+        if np.all(np.diff(row, d) == 0):
+            return d
+
+next_vals = []
+for row in x:
+    d = get_d(row)
+    tot = -sum((-1)**k * comb(d, k, exact=True) * row[-k] for k in range(1, d + 1))
+    next_vals.append(tot)
+
+sol = sum(next_vals)
+print(f"A ::: {sol}")
+
+next_vals = []
+for row in x:
+    row_inv = row[::-1]
+    d = get_d(row_inv)
+    tot = -sum((-1)**k * comb(d, k, exact=True) * row_inv[-k] for k in range(1, d + 1))
+    next_vals.append(tot)
+
+sol = sum(next_vals)
+print(f"B ::: {sol}")
